43f1f687039c2bc65d53aa2d555c2a1152b23af5
admichael1 <techmicado@gmail.com>
2023-08-16 14:36:16 +0100

    config 2

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILE_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join('staticfiles_build', 'static',)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # media files configuration
 MEDIA_URL = '/media/'
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     # ORDERS
     path('orders/', include('orders.urls')),
 ] 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
